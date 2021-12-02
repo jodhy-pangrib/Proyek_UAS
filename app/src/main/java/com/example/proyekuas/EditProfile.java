974b21e8049d644adc8c155d0f73acc129ea0d3e
jcvinz <jonathancalvin21036@gmail.com>
2021-12-02 15:58:43 +0700

    Kamera + XML Baru Edit Profile

--- app/src/main/java/com/example/proyekuas/EditProfile.java
+++ app/src/main/java/com/example/proyekuas/EditProfile.java
@@ -1,14 +1,153 @@
 package com.example.proyekuas;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class EditProfile extends AppCompatActivity {
 
+    private static final int PERMISSION_REQUEST_CAMERA = 100;
+    private static final int CAMERA_REQUEST = 0;
+    private static final int GALLERY_PICTURE = 1;
+
+    private Bitmap bitmap = null;
+    private CircleImageView photoProfile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        photoProfile = findViewById(R.id.photoProfile);
+
+        photoProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater layoutInflater = LayoutInflater.from(EditProfile.this);
+                View selectMediaView = layoutInflater.inflate(R.layout.layout_select_media, null);
+
+                final AlertDialog alertDialog = new AlertDialog.Builder(selectMediaView.getContext()).create();
+
+                Button btnKamera = selectMediaView.findViewById(R.id.btn_kamera);
+                Button btnGaleri = selectMediaView.findViewById(R.id.btn_galeri);
+
+                btnKamera.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (checkSelfPermission(Manifest.permission.CAMERA) ==
+                                PackageManager.PERMISSION_DENIED) {
+                            String[] permission = {Manifest.permission.CAMERA};
+                            requestPermissions(permission, PERMISSION_REQUEST_CAMERA);
+                        } else {
+                            // Membuka kamera
+                            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            startActivityForResult(intent, CAMERA_REQUEST);
+                        }
+                        alertDialog.dismiss();
+                    }
+                });
+
+                btnGaleri.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Membuka galeri
+                        Intent intent = new Intent(Intent.ACTION_PICK,
+                                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        startActivityForResult(intent, GALLERY_PICTURE);
+
+                        alertDialog.dismiss();
+                    }
+                });
+
+                alertDialog.setView(selectMediaView);
+                alertDialog.show();
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_REQUEST_CAMERA) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Membuka kamera
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, CAMERA_REQUEST);
+            } else {
+                Toast.makeText(EditProfile.this, "Permission denied.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (data == null)
+            return;
+
+        if (resultCode == RESULT_OK && requestCode == GALLERY_PICTURE) {
+            Uri selectedImage = data.getData();
+
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(selectedImage);
+                bitmap = BitmapFactory.decodeStream(inputStream);
+            } catch (Exception e) {
+                Toast.makeText(EditProfile.this, e.getMessage(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        } else if (resultCode == RESULT_OK && requestCode == CAMERA_REQUEST) {
+            bitmap = (Bitmap) data.getExtras().get("data");
+        }
+
+        bitmap = getResizedBitmap(bitmap, 512);
+        photoProfile.setImageBitmap(bitmap);
+    }
+
+    private Bitmap getResizedBitmap(Bitmap bitmap, int maxSize) {
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+
+        float bitmapRatio = (float) width / (float) height;
+
+        if (bitmapRatio > 1) {
+            width = maxSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maxSize;
+            width = (int) (height * bitmapRatio);
+        }
+
+        return Bitmap.createScaledBitmap(bitmap, width, height, true);
+    }
+
+    private String bitmapToBase64(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream .toByteArray();
+        String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
+        return encoded;
     }
 }
