fd044cec6585604933d7d5273abdfb86bc39fba7
ASUS <jonathaneverald@gmail.com>
2021-12-03 21:13:10 +0700

    volley reservasi

--- app/src/main/java/com/example/proyekuas/Volley/api/ReservasiApi.java
+++ app/src/main/java/com/example/proyekuas/Volley/api/ReservasiApi.java
@@ -1,7 +1,7 @@
 package com.example.proyekuas.Volley.api;
 
 public class ReservasiApi {
-    public static final String BASE_URL = "https://pbp-web-api.herokuapp.com/api/";
+    public static final String BASE_URL = "https://e04b-125-162-6-89.ngrok.io/api/";
 
     public static final String GET_ALL_URL = BASE_URL + "reservasi";
     public static final String GET_BY_ID_URL = BASE_URL + "reservasi/";
