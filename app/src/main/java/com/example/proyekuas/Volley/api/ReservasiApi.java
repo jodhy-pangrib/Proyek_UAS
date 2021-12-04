8c3fdfc465f4aaccc188cde642104dc08d277ca9
jodhy-pangrib <jodhy.pangrib@gmail.com>
2021-12-05 02:02:50 +0700

    Proyek_UAS

--- app/src/main/java/com/example/proyekuas/Volley/api/ReservasiApi.java
+++ app/src/main/java/com/example/proyekuas/Volley/api/ReservasiApi.java
@@ -1,7 +1,7 @@
 package com.example.proyekuas.Volley.api;
 
 public class ReservasiApi {
-    public static final String BASE_URL = "http://jojojohotel.xyz/public/api/";
+    public static final String BASE_URL = "https://jojojohotel.xyz/public/api/";
 
     public static final String GET_ALL_URL = BASE_URL + "reservasi";
     public static final String GET_BY_ID_URL = BASE_URL + "reservasi/";
